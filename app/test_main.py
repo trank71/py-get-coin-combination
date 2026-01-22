6cd77a83f7a19e44b3219455d0674abf4eec92f4
Ambra <trank13f@gmail.com>
2026-01-22 15:14:13 +0100

    simple test completed to func get_coin_combination

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,17 @@
+import pytest
+
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "coins, result",
+    [
+        pytest.param(1, [1, 0, 0, 0], id="1 panny"),
+        pytest.param(6, [1, 1, 0, 0], id="6 panny"),
+        pytest.param(17, [2, 1, 1, 0], id="17 panny"),
+        pytest.param(50, [0, 0, 0, 2], id="5 panny"),
+    ]
+)
+def test_get_coin_combination(coins: int, result: list) -> None:
+    assert get_coin_combination(coins) == result
